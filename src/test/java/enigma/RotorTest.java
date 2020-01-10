9396df763abb1f06b35cb143d13ecfa429222836
AKOUGBE Henri Joel <akougbe@gmail>
2020-01-10 15:16:14 +0100

    Rédaction test Rotor class

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -7,8 +7,67 @@ import org.junit.Test;
 public class RotorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void toLettertest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Rotor r = new Rotor(c, 1);
+		char lettre = r.toLetter(2);
+		
+		assertEquals('C',lettre);
+		
 	}
+	
+	@Test
+	public void toIndextest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Rotor r = new Rotor(c, 1);
+		int i = r.toIndex('A');		
+		assertEquals(0,i);
+		
+	}
+	
+	@Test 
+	public void advanceTest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Rotor r = new Rotor(c, 1);
+		int p =r.getPosition();
+		p++;
+		r.advance();		
+		assertEquals(p, r.getPosition());		
+	}
+	
+	
+	@Test
+	public void convertForwardTest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Rotor r = new Rotor(c, 1);		
+		int p = r.convertForward(0);
+		
+		
+	}
+	
+	@Test
+	public void convertBackwardTest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Rotor r = new Rotor(c, 1);
+		
+		int p = r.convertBackward(0);
+	}
+	
+	
 
 }
