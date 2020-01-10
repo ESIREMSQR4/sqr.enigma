0b0d5b39cf32b00dbfeb3a2ca34d2d4460122059
ka793501 <ka793501@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-10 15:32:00 +0100

    Ajout de nouveaux Tests

--- src/test/java/enigma/ReflectorTest.java
+++ src/test/java/enigma/ReflectorTest.java
@@ -7,8 +7,18 @@ import org.junit.Test;
 public class ReflectorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertForwardTest() {
+		int c[] = new int[26];
+		for (int i =0; i!=26; i++ ) {
+			c[i]=i;
+		}
+		Reflector r = new Reflector(c);
+		
+		int z = r.convertForward(0);
+		
+		//Quand on met A on doit avoir A
+		// Donc en mettant 0 on doit avoir 0
+		assertEquals(z, 0);		
 	}
 
 }
--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -46,18 +46,22 @@ public class RotorTest {
 	
 	
 	@Test
+	// Test d'intégration 
 	public void convertForwardTest() {
 		int c[] = new int[26];
 		for (int i =0; i!=26; i++ ) {
 			c[i]=i;
 		}
 		Rotor r = new Rotor(c, 1);		
-		int p = r.convertForward(0);
+		int p = r.convertForward(0);		
+		int m = r.convertBackward(p);
 		
+		assertEquals(m, 0);	
 		
 	}
 	
 	@Test
+	// Test d'intégration
 	public void convertBackwardTest() {
 		int c[] = new int[26];
 		for (int i =0; i!=26; i++ ) {
@@ -66,6 +70,8 @@ public class RotorTest {
 		Rotor r = new Rotor(c, 1);
 		
 		int p = r.convertBackward(0);
+		int m = r.convertForward(p);
+		assertEquals(m, 0);
 	}
 	
 	
